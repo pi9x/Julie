0b87743d1b5ef5469624a565a5006ae6eb3c71b1
pi9x <39524931+pi9x@users.noreply.github.com>
2019-11-05 22:17:28 +0700

    Update Program.cs

--- Julie/Program.cs
+++ Julie/Program.cs
@@ -9,7 +9,7 @@ namespace Julie
             Console.WriteLine("Just one day we spent together\n" +
                               "Under the pine trees in the wind\n" +
                               "Like a hopeless wolf seeing his raven\n" +
-                              "I tried to catch all our moments\n" +
+                              "I tried to catch all moments we had\n" +
                               "End of day, 'cause, you would go away.");
         }
     }
