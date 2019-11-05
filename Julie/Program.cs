3caae35d6ee8003cf3d41bebcce725e25914ced2
pi9x <39524931+pi9x@users.noreply.github.com>
2019-11-05 18:07:59 +0700

    Add project files.

---
+++ Julie/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Julie
+{
+    class ForYou
+    {
+        static void Main()
+        {
+            Console.WriteLine("Just a day we spent together\n" +
+                              "Under the pine trees in the wind\n" +
+                              "Like a hopeless wolf seeing his raven\n" +
+                              "I tried to catch all our moments\n" +
+                              "End of day, 'cause, you would go away.");
+        }
+    }
+}
